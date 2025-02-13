68f652973114323be42c5b06fb5bf66d96777a4b
LouieBiongcog <louieseoul09@gmail.com>
2025-02-13 16:36:45 +0800

    mid changes

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -2,38 +2,44 @@
 import MainLayout from '../MainLayout.vue';
 import ProductCard from '../../Components/ProductCard.vue'
 
-const products = [
-  {
-    name: 'Burger Overload',
-    description: 'An epic tower of flavor, where every layer bursts with juicy patties, melted cheese, crispy bacon, and a medley of toppings—too much of a good thing? Not when its this delicious!',
-    image: '/images/burger.jpeg'
-  },
-  {
-    name: 'Pizza',
-    description: 'A mouthwatering masterpiece piled high with melted cheese, savory meats, fresh veggies, and all the toppings your heart desires—its pizza like youve never experienced before!',
-    image: '/images/pizza.jpg'
-  },
-  {
-    name: 'Roasted Chicken',
-    description: 'A delightful Crispilicous Roasted Chicken for family and friends.',
-    image: '/images/roastedChicken.jpeg'
-  },
-  {
-    name: 'Fillet Fillet',
-    description: 'A golden, crispy fillet piled with layers of tender, flaky fish, perfectly seasoned and served with a generous helping of creamy sauces—this is seafood indulgence at its finest!',
-    image: '/images/fishFillet.jpeg'
-  },
-  {
-    name: 'Green Salad',
-    description: 'A vibrant bowl of fresh greens, crisp veggies, and a rainbow of colorful toppings, all tossed in a zesty dressing—light, refreshing, and bursting with flavor in every bite!',
-    image: '/images/greenSalad.jpeg'
-  },
-  {
-    name: 'Chicken Adobo',
-    description: 'A savory symphony of tender chicken simmered in a rich blend of soy sauce, vinegar, garlic, and aromatic spices—Chicken Adobo is a Filipino classic that bursts with bold, mouthwatering flavors in every bite!',
-    image: '/images/chickenAdobo.jpeg'
-  }
-];
+defineProps({
+    products:{
+        type : Array
+    }
+})
+
+// const products = [
+//   {
+//     name: 'Burger Overload',
+//     description: 'An epic tower of flavor, where every layer bursts with juicy patties, melted cheese, crispy bacon, and a medley of toppings—too much of a good thing? Not when its this delicious!',
+//     image: '/images/burger.jpeg'
+//   },
+//   {
+//     name: 'Pizza',
+//     description: 'A mouthwatering masterpiece piled high with melted cheese, savory meats, fresh veggies, and all the toppings your heart desires—its pizza like youve never experienced before!',
+//     image: '/images/pizza.jpg'
+//   },
+//   {
+//     name: 'Roasted Chicken',
+//     description: 'A delightful Crispilicous Roasted Chicken for family and friends.',
+//     image: '/images/roastedChicken.jpeg'
+//   },
+//   {
+//     name: 'Fillet Fillet',
+//     description: 'A golden, crispy fillet piled with layers of tender, flaky fish, perfectly seasoned and served with a generous helping of creamy sauces—this is seafood indulgence at its finest!',
+//     image: '/images/fishFillet.jpeg'
+//   },
+//   {
+//     name: 'Green Salad',
+//     description: 'A vibrant bowl of fresh greens, crisp veggies, and a rainbow of colorful toppings, all tossed in a zesty dressing—light, refreshing, and bursting with flavor in every bite!',
+//     image: '/images/greenSalad.jpeg'
+//   },
+//   {
+//     name: 'Chicken Adobo',
+//     description: 'A savory symphony of tender chicken simmered in a rich blend of soy sauce, vinegar, garlic, and aromatic spices—Chicken Adobo is a Filipino classic that bursts with bold, mouthwatering flavors in every bite!',
+//     image: '/images/chickenAdobo.jpeg'
+//   }
+// ];
 </script>
 
 <template>
